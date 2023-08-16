5ce819175bebb726f04f08d8c2bfbc82323b0eac
Ari Lotter <arilotter@gmail.com>
2023-08-16 10:03:08 -0400

    cdogs pixel perfect demo

--- test/load.js
+++ test/load.js
@@ -1,12 +1,15 @@
-var loadFont = require('load-bmfont')
-global.THREE = require('three')
+var loadFont = require("load-bmfont");
+const { NearestFilter } = require("three");
+global.THREE = require("three");
 
 // A utility to load a font, then a texture
 module.exports = function (opt, cb) {
   loadFont(opt.font, function (err, font) {
-    if (err) throw err
+    if (err) throw err;
     THREE.ImageUtils.loadTexture(opt.image, undefined, function (tex) {
-      cb(font, tex)
-    })
-  })
-}
+      tex.magFilter = NearestFilter;
+      tex.minFilter = NearestFilter;
+      cb(font, tex);
+    });
+  });
+};
--- test/test-2d.js
+++ test/test-2d.js
@@ -8,60 +8,64 @@
   })
  */
 
-global.THREE = require('three')
-var createOrbitViewer = require('three-orbit-viewer')(THREE)
-var createText = require('../')
+global.THREE = require("three");
+var createOrbitViewer = require("three-orbit-viewer")(THREE);
+var createText = require("../");
 
-require('./load')({
-  font: 'fnt/Lato-Regular-64.fnt',
-  image: 'fnt/lato.png'
-}, start)
+require("./load")(
+  {
+    font: "fnt/cdogs.json",
+    image: "fnt/cdogs.png",
+  },
+  start
+);
 
-function start (font, texture) {
+function start(font, texture) {
   var app = createOrbitViewer({
-    clearColor: 'rgb(80, 80, 80)',
+    clearColor: "rgb(110,132,189)",
     clearAlpha: 1.0,
     fov: 65,
-    position: new THREE.Vector3()
-  })
+    position: new THREE.Vector3(),
+  });
 
-  app.camera = new THREE.OrthographicCamera()
-  app.camera.left = 0
-  app.camera.top = 0
-  app.camera.near = -100
-  app.camera.far = 100
+  app.camera = new THREE.OrthographicCamera();
+  app.camera.left = 0;
+  app.camera.top = 0;
+  app.camera.near = -100;
+  app.camera.far = 100;
 
   var geom = createText({
-    text: 'this bitmap text\nis rendered with \nan OrthographicCamera',
+    text: `Touch the Sky`,
     font: font,
-    align: 'left',
-    width: 700,
-    flipY: texture.flipY
-  })
+    align: "left",
+    width: 100,
+    flipY: texture.flipY,
+  });
 
   var material = new THREE.MeshBasicMaterial({
     map: texture,
     transparent: true,
-    color: 'rgb(230, 230, 230)'
-  })
+    color: "rgb(255, 255, 255)",
+  });
 
-  var layout = geom.layout
-  var text = new THREE.Mesh(geom, material)
-  var padding = 40
-  text.position.set(padding, -layout.descender + layout.height + padding, 0)
+  var layout = geom.layout;
+  var text = new THREE.Mesh(geom, material);
+  var padding = 40;
+  text.position.set(padding, -layout.descender + layout.height + padding, 0);
 
-  var textAnchor = new THREE.Object3D()
-  textAnchor.add(text)
-  textAnchor.scale.multiplyScalar(1 / (window.devicePixelRatio || 1))
-  app.scene.add(textAnchor)
+  var textAnchor = new THREE.Object3D();
+  textAnchor.add(text);
+  // textAnchor.scale.multiplyScalar(1 / (window.devicePixelRatio || 1));
+  textAnchor.scale.setScalar(1);
+  app.scene.add(textAnchor);
 
   // update orthographic
-  app.on('tick', function () {
+  app.on("tick", function () {
     // update camera
-    var width = app.engine.width
-    var height = app.engine.height
-    app.camera.right = width
-    app.camera.bottom = height
-    app.camera.updateProjectionMatrix()
-  })
+    var width = app.engine.width;
+    var height = app.engine.height;
+    app.camera.right = width;
+    app.camera.bottom = height;
+    app.camera.updateProjectionMatrix();
+  });
 }
